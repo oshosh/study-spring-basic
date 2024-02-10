63283a85a5cc27ec4d4ad085b473307c76840a16
oshosh <dhtpgus7@gmail.com>
2024-02-10 15:17:21 +0900

    feat: 다형성을 유지하면서 조회한 빈이 모두 필요한 경우 Map혹은 List를 통해서 조회

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,7 +5,6 @@ import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class FixDiscountPolicy implements DiscountPolicy{
   private int discountFixAmount = 1000;
   @Override
